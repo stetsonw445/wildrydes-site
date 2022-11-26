240892e0d4058b19aa2818dd4e54767132ae00e1
stetsonw445 <85764643+stetsonw445@users.noreply.github.com>
2022-11-26 15:28:18 -0600

    Adding Invocation URL for API Gateway

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://j2olr7e21d.execute-api.us-west-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
